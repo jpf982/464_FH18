0fb29715405ef2e5948be2faad0c02471e1f4413
Vivian Rogers <vivian.rogers@utexas.edu>
2022-01-28 04:16:14 -0600

    Changed Chip.py to PhQ.py and added a waffling monologue

---
+++ storage/authenticate/PhQ.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+from copy import *
+
+
+
+
+#PhQ = namedtuple('PhQ', 'name identity Tvals freqvals npoints maxf minf')
+
+# ugh I am conflicted if this should be a named tuple or object.
+# I think object makes more sense + is more secure, I will explain on a call. 
+# but my C/julia/haskell brain hates hates hates this
+
+# NEVERMIND this is temporary, but needs to be handled as a namedtuple in the end
+# before it is put in the database.
+class PhQ:
+    def __init__(self, _name, _identity, _freqvals, _Tvals, ):
+        self.name = _name
+        self.identity = _identity
+        self.Tvals = deepcopy(_Tvals)
+        self.freqvals = deepcopy(_freqvals)
+        self.npoints = _freqvals.size
+        self.maxf = np.amax(_freqvals)
+        self.minf = np.amin(_freqvals)
+
+
+
+
+
