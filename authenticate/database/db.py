Fixed transmission normalization

authenticate/database/db.py
@@ -39,7 +39,8 @@ class database:
         """
         try:
             name, freqVals, tVals = key.getValues()
-            maxT = maximum(tVals)
+            # normalize to largest value in transmission spectrum
+            maxT = np.amax(tVals)
             tVals = (1/maxT)*tVals
             spectrum = pd.DataFrame({'FreqVals': freqVals, 'TVals': tVals})
             spectrum.loc[len(spectrum.index)] = [-1.0, -1.0]